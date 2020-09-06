56981f4da81ffed9e3c06c300f8ea8f0d87b715c
D. Lyons <d.b.lyons@outlook.com>
2020-09-06 11:48:38 -0500

    Added boilerplate backend

---
+++ cgg-backend/src/main/java/com/aandd/cggbackend/CggBackendApplication.java
@@ -0,0 +1,12 @@
+package com.aandd.cggbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CggBackendApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(CggBackendApplication.class, args);
+	}
+
+}
---
+++ cgg-backend/src/test/java/com/aandd/cggbackend/CggBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aandd.cggbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CggBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
